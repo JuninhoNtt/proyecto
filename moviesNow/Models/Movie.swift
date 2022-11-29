7489b12f173f1cf4d272a3124890ccb26567259a
Juninho <jcherohi@emeal.nttdata.com>
2022-11-29 11:18:22 -0500

    llenando la lista

--- moviesNow/Models/Movie.swift
+++ moviesNow/Models/Movie.swift
@@ -17,7 +17,13 @@ extension Movie{
     
     static var dataMovie = [
     Movie(titulo: "batman", valoracion: "me parece una buena pelicula"),
-    Movie(titulo: "superman", valoracion: "me parece una mala pelicula")
-    Movie(titulo: "el padrino", valoracion: "para ver en familia")
+    Movie(titulo: "superman", valoracion: "me parece una mala pelicula"),
+    Movie(titulo: "el padrino", valoracion: "para ver en familia"),
+    Movie(titulo: "titanic", valoracion: "pelicula aburrida"),
+    Movie(titulo: "cars", valoracion: "mal cgi"),
+    Movie(titulo: "el padrino 2", valoracion: "para ver un fin de semana"),
+    Movie(titulo: "la mujer maravilla", valoracion: "buena peli"),
+    Movie(titulo: "aquamman", valoracion: "buen cgi"),
+    Movie(titulo: "radipos y furiosos", valoracion: "muchos autos")
     ]
 }
